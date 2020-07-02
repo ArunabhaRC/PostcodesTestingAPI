bbb1d5ea39d48141f1d86b2a1db2f233b0fc204a
Raja Roy chowdhury <raja@rajas-mbp.home>
2020-07-02 19:24:49 +0100

    Updating all classes and adding body of HeaderDTO,

--- src/test/java/org/example/HeaderDTO.java
+++ src/test/java/org/example/HeaderDTO.java
@@ -1,29 +1,45 @@
 package org.example;
 
-import java.io.IOException;
 import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
-import java.net.URL;
 
 public class HeaderDTO {
 
     private HttpURLConnection httpURLConnection;
 
-    public HeaderDTO(String link){
+    public HeaderDTO(String link) {
         JacksonFileReader fileReader = new JacksonFileReader();
         httpURLConnection = fileReader.readAPIHeaders(link);
     }
 
-
-    public String getServer(){
+    public String getServer() {
         return httpURLConnection.getHeaderField("Server");
     }
 
-    public String getDayDateTime(){
+    public String getDayDateTime() {
         return httpURLConnection.getHeaderField("Date");
     }
 
+    public String getContentType() {
+        return httpURLConnection.getHeaderField("Content-Type");
+    }
 
+    public String getContentlength() {
+        return httpURLConnection.getHeaderField("Content-Length");
+    }
 
+    public String getConnection() {
+        return httpURLConnection.getHeaderField("Connection");
+    }
 
+    public String getXGNU() {
+        return httpURLConnection.getHeaderField("X-GNU");
+    }
+
+    public String getAccessControlAllowOrigin() {
+        return httpURLConnection.getHeaderField("Access-Control-Allow-Origin");
+    }
+
+    public String getETag() {
+        return httpURLConnection.getHeaderField("Etag");
+    }
 }
